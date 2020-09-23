fc6548b5f53a6b4acd36164d9676b0cb5a093499
Arash4567 <shomoyxhacker007@mail.ru>
2020-09-23 12:30:41 +0500

    .

--- scalaTutorials.scala
+++ scalaTutorials.scala
@@ -1,12 +1,17 @@
 import scala.io.StdIn._
+// import java.util.Date
 
 object scalaTutorials {
-  object Math {
-    def divide(x: Int, y: Int) = x / y
-  }
-  def add(x: Int, y: Int): Int = {
-    return x + y
-  }
+  // object Math {
+  //   def divide(x: Int, y: Int) = x / y
+  //   def **(x: Int) = x * x
+  // }
+  // def add(x: Int, y: Int): Int = {
+  //   return x + y
+  // }
+  // def log(date: Date, message: String) {
+  //   println(date + " " + message)
+  // }
   def main(args: Array[String]) {
     // var x = 0
     // while (x < 10) {
@@ -57,5 +62,16 @@ object scalaTutorials {
     // }
     // println(add(2, 4))
     // println(Math.divide(4, 2))
+    // println(Math.**(5))
+
+    /*Anonymus function*/
+    // var add = (x: Int, y: Int) => x+y
+    // println(add(100,500))
+    // val sum = (a: Int, b: Int, c: Int) => a + b + c
+    // var j = sum(45, 32, _: Int)
+    // println(j(12))
+    // val date = new Date
+    // val newLog= log(date, _:String)
+    // newLog("Uzbekistan")
   }
 }
